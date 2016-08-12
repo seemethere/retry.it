aab6a9c1abe1f90b03a3ee7a460f0376127aac31
Eli Uriegas <seemethere101@gmail.com>
2016-08-11 20:56:59 -0500

    Adding some tests

---
+++ test_retry.py
@@ -0,0 +1,35 @@
+import pytest
+
+import retry
+
+exception_message = 'testing exceptions'
+
+
+def foo(bar):
+    if bar < 0:
+        raise ArithmeticError(exception_message)
+    return bar
+
+
+def test_success_criteria():
+    foo_with_success = retry.retry(success=lambda x: x > 0)(foo)
+    with pytest.raises(retry.MaximumRetriesExceeded):
+        foo_with_success(0)
+
+
+def test_exception_criteria():
+    foo_with_exception = retry.retry(exceptions=(ArithmeticError,))(foo)
+    with pytest.raises(ArithmeticError) as exc_info:
+        foo_with_exception(-1)
+    assert exception_message in str(exc_info.value)
+
+
+def test_execution():
+    foo_with_both = retry.retry(
+        exceptions=(ArithmeticError,), success=lambda x: x > 0)(foo)
+    assert foo_with_both(1) == 1
+
+
+def test_invalid_parameters():
+    with pytest.raises(TypeError):
+        retry.retry(exceptions=None, success=None)(foo)
