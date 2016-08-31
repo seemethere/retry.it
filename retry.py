6850629f9a3c5d23b35a27dad8c59f7e067c637d
Eli Uriegas <seemethere101@gmail.com>
2016-08-31 15:16:38 -0500

    Incrementing version

--- retry.py
+++ retry.py
@@ -7,7 +7,7 @@ import time
 
 from decorator import decorator
 
-__version__ = '1.0'
+__version__ = '1.2'
 __author__ = 'Eli Uriegas'
 
 _retries_error_msg = ('Exceeded maximum number of retries {} at '
